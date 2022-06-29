84fafa599b07ed5322161ec781ac2aa9e2de6d19
Pradhu007 <55123592+Pradhu007@users.noreply.github.com>
2022-06-29 14:48:54 +1000

    Update BatteryMonitor.py

--- BatteryMonitor.py
+++ BatteryMonitor.py
@@ -4,14 +4,6 @@ import tkinter.messagebox
 import psutil
 # Importing these modules to assist in the development of this application. 
 
-
-
-    
-    
-    
-
-
-
 def checkifchargerispluggedin():
     plugged = False
 
@@ -24,24 +16,17 @@ def checkifchargerispluggedin():
         return False
 # A function that detects if a poweradapter is plugged into the system
 
-
-
-
 def getosversion():
 
       plat = platform.system()
       if plat == "Windows":
-          tkinter.messagebox.showinfo("Battery Monitoro","Hello Windows User Welcome to the battery monitoring system")
+          tkinter.messagebox.showinfo("Battery Monitoro","Hello Windows User Welcome to the battery monitoring system!")
       elif plat == "Linux":
-          print("Hello Linux User Welcome to the battery monitoring system")
+         tkinter.messagebox.showinfo("Battery Monitoro","Hello Linux  User to the battery monitoring system!")
           
           # A function that fetches the operating system the user machine is running the program on. 
           
           
-     
-
-
-
 def main():
     counter = 0 # This counter has a special need in the program as it prevents the first condition from executing multiple times and therefore to improve user convenience 
   
@@ -73,12 +58,14 @@ def main():
 
         elif f.percent <= 20 and checkifchargerispluggedin() == False:
             tkinter.messagebox.showinfo("Your battery is critically low at a low {} ",format(f.percent))
-
+          # These conditions monitor the status of the battery regularly and report it to the user as soon as something has occured. 
 
         time.sleep(20)
         # This sleep timer has been implemented to allow the user to respond to the result of the program. The sleep time is set to 20 seconds but this has to  be manually customized 
         # TODO - Autommatically customize the timer function so that the user can choose the delay second. 
-        
+        # TODO- Add customization so that the user can choose whether to add the application to startup. To add this we need add a function called isstartup and make it to return a value like a True or a false depending on the user choice.
+        #TODO- NEED TO FIX FORMATTING and add  a better messagebox reply back to the user!
+        #TODO - ADD A RANDOM QUOTE GENERATOR 
 
 
 
