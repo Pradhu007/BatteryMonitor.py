2d0d862a26fc42f723c884aadca5dab269c56fd1
Pradhu007 <55123592+Pradhu007@users.noreply.github.com>
2024-09-13 20:31:02 +1000

    Update BatteryMonitor.py

--- BatteryMonitor.py
+++ BatteryMonitor.py
@@ -4,93 +4,54 @@ import tkinter.messagebox
 import psutil
 
 
-# Importing these modules to assist in the development of this application.
-
-def checkifchargerisplugg():
-    plugged = False
-
-    if psutil.sensors_battery().power_plugged == True:
-        plugged = True
-
-    if plugged == True:
-        return True
-    else:
-        return False
+def is_charger_plugged():
+    """Check if the power adapter is plugged in."""
+    return psutil.sensors_battery().power_plugged
 
 
-# A function that detects if a poweradapter is plugged into the system
-
-def getosversion():
-    plat = platform.system()
-    if plat == "Windows":
-        tkinter.messagebox.showinfo("Battery Monitoro", "Hello Windows User Welcome to the battery monitoring system!")
-    elif plat == "Linux":
-        tkinter.messagebox.showinfo("Battery Monitoro", "Hello Linux  User to the battery monitoring system!")
-
-        # A function that fetches the operating system the user machine is running the program on.
+def show_os_version():
+    """Display the user's operating system and greet them."""
+    os_name = platform.system()
+    if os_name == "Windows":
+        tkinter.messagebox.showinfo("Battery Monitor", "Hello Windows User! Welcome to the battery monitoring system.")
+    elif os_name == "Linux":
+        tkinter.messagebox.showinfo("Battery Monitor", "Hello Linux User! Welcome to the battery monitoring system.")
 
 
 def main():
-    counter = 0# This counter has a special need in the program as it prevents the first condition from executing multiple times and therefore to improve user convenience
-    powercounter = 0 #The power counter stops the need for the power is plugged in condition to keep executing as when the power counter is larger than zero,  it just continues the iteration which prevents the messagebox from appearing again and again.
+    battery_full_notified = False  # To prevent multiple notifications for 100% battery
+    charger_plugged_notified = False  # To avoid repeat notifications when charger is plugged in
+
     while True:
-        # A while loop has been set to continously monitor the battery system on the machine
+        battery = psutil.sensors_battery()
 
-        f = psutil.sensors_battery()
-        # Stores the current battery percent after each iteration
+        # Notify when battery reaches 100%
+        if battery.percent == 100 and not battery_full_notified:
+            tkinter.messagebox.showinfo("Battery Monitor", "Battery is fully charged at 100%!")
+            battery_full_notified = True
 
-        if f.percent == 100 and counter == 0:
-            tkinter.messagebox.showinfo("Advanced battery monitoro", "What a great day to start the day with 100%!")
-            counter = counter + 1
+        # Notify when charger is plugged in and reset notification flags
+        if is_charger_plugged() and not charger_plugged_notified:
+            tkinter.messagebox.showinfo("Battery Monitor", "Charger plugged in.")
+            charger_plugged_notified = True
+            battery_full_notified = False  # Reset for next charge cycle
 
-            if counter > 0:
-                continue
+        # Notify when battery drops to 50% or below and charger is unplugged
+        if battery.percent <= 50 and not is_charger_plugged():
+            charger_plugged_notified = False  # Reset if charger gets unplugged
+            tkinter.messagebox.showinfo("Battery Monitor", "Battery at 50%. Please plug in the charger.")
 
-        elif checkifchargerisplugg() == False and powercounter == 0:
-            tkinter.messagebox.showinfo("Advanced battery monitoro", "Power plugged in")
-            powercounter = powercounter + 1
+        # Notify when battery drops to 30% or below
+        if battery.percent <= 30 and not is_charger_plugged():
+            tkinter.messagebox.showinfo("Battery Monitor", "Battery at 30%. Please plug in the charger soon!")
 
-            if powercounter > 0:
-                continue
-        elif f.percent <= 50 and checkifchargerisplugg() == False:
-            powercounter = 0
+        # Notify when battery drops to 20% or below
+        if battery.percent <= 20 and not is_charger_plugged():
+            tkinter.messagebox.showinfo("Battery Monitor", "Battery at 20%! Consider backing up your data and plugging in the charger.")
 
-            tkinter.messagebox.showinfo("Advanced battery monitoro",
-                                        "Your battery is now currently at 50%. Please consider plugging your charger")
-            
-        
-
-        elif f.percent <= 30 and checkifchargerisplugg() == False:
-           powercounter = 0
-           tkinter.messagebox.showinfo("Advanced battery monitoro","Please consider plugging in charger and consider backing up data! ")
-            
-        elif f.percent <= 20 and checkifchargerisplugg() == False:
-            powercounter = 0
-        tkinter.messagebox.showinfo("Advanced battery monitoro","Please consider plugging in charger and consider backing up data! ")
-        
-
-       
-# These conditions monitor the status of the battery regularly and report it to the user as soon as something has occured.
-
-time.sleep(20)
-# This sleep timer has been implemented to allow the user to respond to the result of the program. The sleep time is set to 20 seconds but this has to  be manually customized
-# TODO - Autommatically customize the timer function so that the user can choose the delay second.
-# TODO- Add customization so that the user can choose whether to add the application to startup. To add this we need add a function called isstartup and make it to return a value like a True or a false depending on the user choice.
-# TODO- NEED TO FIX FORMATTING and add  a better messagebox reply back to the user!
-# TODO - ADD A RANDOM QUOTE GENERATOR
+        time.sleep(20)  # Delay between checks
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    show_os_version()
+    main()
